278ce3d87c8b65cfcf9bd8ac6e72e9475cdadbb8
Vance Winstead <vance.winstead@gmail.com>
2022-10-23 15:51:41 -0700

    refactor: linting fixes

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -4,5 +4,5 @@ import App from "../App";
 test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/Cleanedup React App/i);
-  // expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeInTheDocument();
 });
