bab35b03e98a4d7f13e9dad503fb1726a0c4f6a1
andrewtchan <andrew2020chan@gmail.com>
2023-02-27 12:35:25 -0800

    refactor: destructuring props assignment

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,26 +1,27 @@
+import "./ItemCard.css";
+
 interface ItemCardProps {
   itemName: string;
   date: string;
   money: string;
-  tags: string[];
-  imgSrc: string;
+  //   tags: string[];
 }
 
-export default function ItemCard(props: ItemCardProps) {
+export default function ItemCard({ itemName, date, money }: ItemCardProps) {
   return (
     <div className="item-card">
-      <img className="item-card-img" src={props.imgSrc} alt="itemName" />
+      <div className="item-card-imgbox" />
       <div className="item-card-info">
         <div className="item-titlebox">
-          <h1 className="item-card-name">{props.itemName}</h1>
-          <h1 className="item-card-date">{props.date}</h1>
+          <p className="item-card-name">{itemName}</p>
+          <p className="item-card-date">{date}</p>
         </div>
-        <p className="item-card-price">{props.money}</p>
-        <div className="item-card-categories">
+        <p className="item-card-price">{money}</p>
+        {/* <div className="item-card-categories">
           {props.tags.map((tag) => (
             <p className="item-card-tag">{tag}</p>
           ))}
-        </div>
+        </div> */}
       </div>
     </div>
   );
