c8ae6214e62241a8e132fe535344b541320831a7
upluke <lukeforadream@gmail.com>
2023-03-11 20:04:33 -0800

    debugged auth config and Manifest icon issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from "react";
 import "./App.css";
-import { Amplify } from "aws-amplify";
+import { Auth } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
 import { Authenticator } from "@aws-amplify/ui-react";
-import awsExports from "./aws-exports";
+import awsconfig from "./aws-exports";
+// import awsconfig from './aws-exports';
 
+Auth.configure(awsconfig);
 // Amplify.configure(awsExports);
 
-Amplify.configure({
-  Auth: {
-    region: awsExports.aws_project_region,
-    userPoolId: awsExports.aws_user_pools_id,
-    userPoolWebClientId: awsExports.aws_user_pools_web_client_id,
-  },
-});
+// Amplify.configure({
+//   Auth: {
+//     region: awsExports.aws_project_region,
+//     userPoolId: awsExports.aws_user_pools_id,
+//     userPoolWebClientId: awsExports.aws_user_pools_web_client_id,
+//   },
+// });
 
 function App() {
   return (
     <Authenticator>
       {({ signOut, user }: any) => (
         <div>
-          <p>Welcome {user.username}</p>
+          <p>Welcome to wilshire, {user.username}</p>
           <button type="button" onClick={signOut}>
             Sign out
           </button>
