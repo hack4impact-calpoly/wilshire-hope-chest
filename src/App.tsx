eded71a9e5427a761d8d5df283b96aaf7615bcd2
Jonathan Laksana <jonathanalaksana@gmail.com>
2023-03-12 16:46:19 -0700

    flexbox!

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import "./App.css";
+import ItemCard from "./components/ItemCard";
+import TagsList from "./components/TagsList";
 
 function App() {
-  return <p>Wilshire Hospice App</p>;
+  return (
+    <div>
+      <TagsList categories={["tag1", "tag2", "tag3"]} />
+      <ItemCard
+        itemName="name"
+        date={new Date()}
+        money={5.6}
+        tags={["tag1", "tag2", "tag3"]}
+      />
+      <ItemCard
+        itemName="name"
+        date={new Date()}
+        money={5.6}
+        tags={["tag1", "tag2", "tag3"]}
+      />
+      <ItemCard
+        itemName="name"
+        date={new Date()}
+        money={5.6}
+        tags={["tag1", "tag2", "tag3"]}
+      />
+    </div>
+  );
 }
 
 export default App;
