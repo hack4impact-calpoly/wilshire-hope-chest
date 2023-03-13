4e5b620d1fa1bf4ba6c194c1f22adec34a1f98d3
andrewtchan <andrew2020chan@gmail.com>
2023-03-12 17:36:04 -0700

    reverted App.tsx changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,7 @@
 import "./App.css";
-import ItemCard from "./components/ItemCard";
-import TagsList from "./components/TagsList";
 
 function App() {
-  return (
-    <div>
-      <TagsList categories={["tag1", "tag2", "tag3"]} />
-      <ItemCard
-        itemName="name"
-        date={new Date()}
-        money={5.6}
-        tags={["tag1", "tag2", "tag3"]}
-      />
-      <ItemCard
-        itemName="name"
-        date={new Date()}
-        money={5.6}
-        tags={["tag1", "tag2", "tag3"]}
-      />
-      <ItemCard
-        itemName="name"
-        date={new Date()}
-        money={5.6}
-        tags={["tag1", "tag2", "tag3"]}
-      />
-    </div>
-  );
+  return <p>Wilshire Hospice App</p>;
 }
 
 export default App;
