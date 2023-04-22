3635875c8b9a47615335cdae461b1724a826dae9
upluke <lukeforadream@gmail.com>
2023-04-21 22:31:01 -0700

    testing stage

--- src/components/Dropdown/StyleDropdown.tsx
+++ src/components/Dropdown/StyleDropdown.tsx
@@ -8,16 +8,35 @@ import { Category } from "../../models";
 export default function Styledropdown() {
   const [icon, setIcon] = useState(<BiPlus size={18} />);
   const [categories, setCategories] = useState<Category[]>([]);
+  const seed = [
+    { name: "test_category_1", description: "first testing category" },
+    { name: "test_category_2", description: "second testing category" },
+    { name: "test_category_3", description: "thrid testing category" },
+  ];
 
   useEffect(() => {
     async function fetchCategories() {
       const fetchedCategories = await DataStore.query(Category);
       setCategories(fetchedCategories);
+
+      // const existingCatNames = fetchCategories.map((el) => el.name);
+
+      // seed.forEach(async (el) => {
+      //   if (!existingCatNames.includes(el.name)) {
+      //     await DataStore.save(
+      //       new Category({
+      //         ...el,
+      //       })
+      //     );
+      //   }
+      // });
+
+      // await DataStore.delete(Category, (category) => category.name.eq("test")); //delete all the posts
     }
     fetchCategories();
   }, []);
 
-  console.log(categories);
+  console.log(categories, seed);
 
   function Showhide() {
     const click = document.getElementById("StyleDropdown");
@@ -29,11 +48,31 @@ export default function Styledropdown() {
       setIcon(<BiPlus size={18} />);
     }
   }
+
+  const handleSeed = async () => {
+    seed.forEach(async (el) => {
+      await DataStore.save(
+        new Category({
+          ...el,
+        })
+      );
+    });
+  };
+
+  const handleDeleteSeed = async () => {
+    await DataStore.delete(Category, (category) => category.name.eq("test")); // delete all the posts
+  };
   return (
     <div className="dropdown">
       <button type="button" onClick={Showhide} className="dropbtn">
         <span id="Change"> {icon} </span>
         <span className="C1"> Style </span>
+      </button>
+      <button type="button" className="test" onClick={handleSeed}>
+        Seed
+      </button>
+      <button type="button" className="test" onClick={handleDeleteSeed}>
+        Delete Seed
       </button>
       <div id="StyleDropdown" className="dropdown-content-three-items">
         <div className="font">
