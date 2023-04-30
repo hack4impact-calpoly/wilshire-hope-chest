39b2714bc2e49085848a85ee99e113a96886cb95
Eeshan Walia <eeshanwalia@gmail.com>
2023-04-29 17:41:15 -0700

    feat: reload on sign-out

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,13 @@
-import { useState } from "react";
 import { Auth } from "aws-amplify";
 import "../App.css";
 import "./Home.css";
 import Header from "./Header";
 
 function Home() {
-  const [refreshPage, setRefreshPage] = useState(false);
-
   async function signOut() {
     try {
       await Auth.signOut();
-      setRefreshPage(!refreshPage);
+      window.location.reload();
     } catch (error) {
       console.log("error signing out: ", error);
     }
