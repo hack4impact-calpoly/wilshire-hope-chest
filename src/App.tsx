161f2700c731510ab6f767469016f27030edfb74
Jonathan Laksana <jonathanalaksana@gmail.com>
2023-05-25 10:53:02 -0700

    revert app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-// import { Authenticator } from "@aws-amplify/ui-react";
+import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import { Auth } from "aws-amplify";
 import "./App.css";
 import awsconfig from "./aws-exports";
-// import Home from "./components/Home";
-// import AddItemButton from "./components/addItems/AddItemButton";
-import Report from "./components/Report";
+import Home from "./components/Home";
+import AddItemButton from "./components/addItems/AddItemButton";
 
 // import configuration of AWS authentication
 Auth.configure(awsconfig);
 
 function App() {
   return (
-    // <Authenticator>
-    //   <Home />
-    //   <AddItemButton />
-    // </Authenticator>
-    <Report />
+    <Authenticator>
+      <Home />
+      <AddItemButton />
+    </Authenticator>
   );
 }
 
